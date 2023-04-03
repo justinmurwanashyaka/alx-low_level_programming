8de9e429f1bdb025a05e6bd86a6ebbc04b73d096
justin <justin18482@gmail.com>
2023-04-03 06:52:26 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function that will prints the albhabet in lowcase
+ * flowed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int a =97;
+
+	while (a <= 227)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
